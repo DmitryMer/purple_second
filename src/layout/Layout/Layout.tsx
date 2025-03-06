43268e13121daea9472c468b57faca7d3f64beaa
Dmitry <duduff9317@gmail.com>
2025-03-06 17:16:56 +0300

    update: add link and navLink

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,10 +1,11 @@
-import { Link, Outlet } from "react-router";
+import { Outlet, NavLink } from "react-router";
 import styles from "./Layout.module.css";
 import avatar from "../../public/avatar.png";
 import menuicon from "../../public/menu-icon.svg";
 import carticon from "../../public/cart-icon.svg";
 import exiticon from "../../public/exit-icon.svg";
 import Button from "../../components/Button/Button";
+import cn from "classnames";
 
 export function Layout() {
   return (
@@ -20,14 +21,28 @@ export function Layout() {
           <div className={styles["email"]}>alaricode@ya.ru</div>
         </div>
         <div className={styles["menu"]}>
-          <Link to="/" className={styles["link"]}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: isActive,
+              })
+            }
+          >
             <img src={menuicon} alt="Иконка меню" />
             Меню
-          </Link>
-          <Link to="/cart" className={styles["link"]}>
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              cn(styles["link"], {
+                [styles.active]: location.pathname === "/cart",
+              })
+            }
+          >
             <img src={carticon} alt="Иконка корзины" />
             Корзина
-          </Link>
+          </NavLink>
         </div>
         <Button className={styles["exit"]}>
           <img src={exiticon} alt="Иконка выхода" />
